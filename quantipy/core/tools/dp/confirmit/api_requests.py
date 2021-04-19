5b2fd20a550e71af5b2ff65a72251530c16674ab
pablo <pablodo@gmail.com>
2021-04-19 10:30:44 -0300

    Api reqs & writer files

---
+++ quantipy/core/tools/dp/confirmit/api_requests.py
@@ -0,0 +1,71 @@
+import asyncio
+import requests as req
+import json
+
+
+async def get_surveys(projectid, public_url, idp_url, client_id, client_secret, schema_vars, schema_filter):
+    # Source configuration
+    source_projectid = projectid
+    source_public_site_url = public_url
+    source_idp_url = idp_url
+    source_client_id = client_id
+    source_client_secret = client_secret
+    data_params = {}
+    meta_params = {}
+    if schema_vars:
+        data_params['variables'] = schema_vars
+        meta_params['variables'] = schema_vars
+
+    if schema_filter:
+        data_params['filterExpression'] = schema_filter
+
+    def get_token(source_idp_url, source_client_id, source_client_secret):
+        # Get access token
+        response = req.post(source_idp_url + 'identity/connect/token',
+                            data="grant_type=api-user&scope=pub.surveys",
+                            auth=(source_client_id, source_client_secret),
+                            headers={'Content-Type': 'application/x-www-form-urlencoded'})
+        response.raise_for_status()
+        resp_obj = response.json()
+        return resp_obj['access_token']
+
+
+    async def get_survey_data(source_token, source_public_site_url, source_projectid):
+        # Get source data records
+        headers = {'Authorization': 'Bearer ' + source_token, "Accept": "application/x-ndjson", "Content-Type": "application/json"}
+        url = source_public_site_url + 'v1/surveys/' + source_projectid + '/responses/data'
+        response = req.get(url, params=data_params, headers=headers, stream=False)
+        response.raise_for_status()
+
+        # Decode json response - data
+        res = response.content.decode("utf-8")
+        json_lines = res.splitlines()
+        json_data = []
+        for line in json_lines:
+            json_data.append(json.loads(line))
+        return json_data
+
+
+    async def get_survey_meta(source_token, source_public_site_url, source_projectid):
+        # Get survey schema records
+        headers = {'Authorization': 'Bearer ' + source_token, "Accept": "application/json", "Content-Type": "application/json"}
+        url = source_public_site_url + 'v1/surveys/' + source_projectid + '/responses/schema'
+        response_schema = req.get(url, params=meta_params, headers=headers, stream=False)
+        response_schema.raise_for_status()
+        # Decode json response - schema
+        res = response_schema.content.decode("utf-8")
+        json_lines = res.splitlines()
+        json_meta = []
+        for line in json_lines:
+            json_meta.append(json.loads(line))
+        return json_meta
+
+
+    source_token = get_token(source_idp_url, source_client_id, source_client_secret)
+    task1 = asyncio.create_task(get_survey_data(source_token, source_public_site_url, source_projectid))
+    task2 = asyncio.create_task(get_survey_meta(source_token, source_public_site_url, source_projectid))
+
+    json_data = await task1
+    json_meta = await task2
+
+    return json_data, json_meta
---
+++ quantipy/core/tools/dp/confirmit/writer.py
@@ -0,0 +1,5 @@
+import json
+
+def quantipy_to_confirmit(path_meta, path_data, meta, data, schema_vars, verbose):
+    a = 10
+    return a, 'b'
